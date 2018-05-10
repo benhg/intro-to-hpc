1d98a77f8be46c24c98ce8d634d8a861a9d06034
Ben Glick <glick@lclark.edu>
2018-05-10 18:58:44 -0400

    finished notebook 2, added data as necessary

---
+++ data/submit_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo h3ll0 >> /home/users/glick/intro-to-hpc/data/job_out.out
+sleep 5
+echo h3ll0 >> /home/users/glick/intro-to-hpc/data/job_out.out
---
+++ data/submit_script.sh~
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+date
+sleep 5
+date
